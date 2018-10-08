52fd073c8733294acad55f4a0e5e8df605ce4fd9
Mota <maike.mota@atos.net>
2018-10-08 16:41:26 -0300

    Added downloadFromMemory and dataURtoBlob
     downloadFromMemory - Takes a base64 or blob and forces the download by the user browser
    
     dataURItoBlob - Takes a base64 and convert that into a Blob

--- src/app/utils/FileUtil.ts
+++ src/app/utils/FileUtil.ts
@@ -14,4 +14,36 @@ export abstract class FileUtil {
         }
         return zip.generateAsync({ type: 'blob' });
     }
+
+    public static downloadFromMemory(fileName: string, fileData: string | Blob) {
+        let a = document.createElement("a");
+        document.body.appendChild(a);
+        let data: Blob = fileData instanceof Blob ? fileData : FileUtil.dataURItoBlob(<string>fileData);
+        let url = window.URL.createObjectURL(data);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        document.body.removeChild(a)
+        window.URL.revokeObjectURL(url);
+    }
+
+    public static dataURItoBlob(dataURI: string) {
+        // convert base64 to raw binary data held in a string
+        // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+        var byteString = atob(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+        // write the bytes of the string to an ArrayBuffer
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        // write the ArrayBuffer to a blob, and you're done
+        var bb = new Blob([ab], { type: mimeString });
+        return bb;
+    }
 }
